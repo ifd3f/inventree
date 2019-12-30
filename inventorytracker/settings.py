3a545580693d842852e261354403c08f323ee203
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-12-29 23:46:20 -0800

    Add models and a test page

--- inventorytracker/settings.py
+++ inventorytracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventory.apps.InventoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'inventorytracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'inventorytracker/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
