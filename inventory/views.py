117dcb8e5209826a0472840f03c8beb7826ec316
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-12-30 14:55:22 -0800

    Item sourcing

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Sum, Count
+from django.db.models import Sum, Count, F
 from django.http import HttpResponse
 from django.template import loader
 
@@ -21,18 +21,17 @@ def search(request):
 
 def index(request):
     template = loader.get_template('index.html')
+    restock_query = Item.objects.all().filter(quantity__lte=F('alert_quantity'))
     context = {
         'items': Item.objects.annotate(total_count=Sum('quantity')),
-        'containers': Container.objects
+        'containers': Container.objects,
+        'restock_items': restock_query
     }
+    print(context)
     return HttpResponse(template.render(context, request))
 
 
 def add(request):
     template = loader.get_template('add.html')
-    context = {
-        'items': Item.objects.annotate(total_count=Sum('quantity')),
-        'containers': Container.objects
-    }
     return HttpResponse(template.render(context, request))
 
