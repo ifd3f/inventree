0c3bdaddf2e47eac8f004d930d498c6f79cd7a8b
Astrid Augusta Yu <astridyu3.14@gmail.com>
2019-12-30 15:54:49 -0800

    Detail pages for containers and items

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('add', views.add, name='add'),
     path('search', views.search, name='search'),
+    path('item/<int:item_id>', views.item_detail, name='item'),
+    path('container/<int:container_id>', views.container_detail, name='container'),
+    path('', views.index, name='index'),
 ]
 
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Sum, Count, F
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from inventory.forms import SearchForm
@@ -9,18 +10,15 @@ from inventory.models import Item, Container
 def search(request):
     form = SearchForm(request.GET)
     if form.is_valid():
-
-        template = loader.get_template('search.html')
         context = {
             'items': Item.objects.annotate(total_count=Sum('quantity')),
             'containers': Container.objects,
             'results': form.get_results()
         }
-        return HttpResponse(template.render(context, request))
+        return render(request, 'search.html', context)
 
 
 def index(request):
-    template = loader.get_template('index.html')
     restock_query = Item.objects.all().filter(quantity__lte=F('alert_quantity'))
     context = {
         'items': Item.objects.annotate(total_count=Sum('quantity')),
@@ -28,10 +26,19 @@ def index(request):
         'restock_items': restock_query
     }
     print(context)
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html', context)
 
 
 def add(request):
-    template = loader.get_template('add.html')
-    return HttpResponse(template.render(context, request))
+    return render(request, 'add.html', {})
+
+
+def item_detail(request, item_id):
+    context = {'item': Item.objects.get(id=item_id)}
+    return render(request, 'item.html', context)
+
+
+def container_detail(request, container_id):
+    context = {'container': Container.objects.get(id=container_id)}
+    return render(request, 'container.html', context)
 
