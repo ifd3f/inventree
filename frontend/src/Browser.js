1255e526d10eb52f296761ab64109f2564f04548
Max <maxim.yu314@gmail.com>
2020-01-02 17:25:23 -0800

    Container passed up to browser and down to viewer

--- frontend/src/Browser.js
+++ frontend/src/Browser.js
@@ -9,11 +9,35 @@ const NODE_LOADING = 1
 const NODE_EXPANDED = 2
 
 
+class ContainerViewer extends Component {
+  constructor(props) {
+    super(props);
+    this.container = props.container;
+  }
+
+  componentWillReceiveProps(props) {
+    this.container = props.container;
+  }
+
+  render() {
+    if (this.container == null) {
+      return <div>
+        <h1 className="text-muted">Select a container on the left.</h1>
+      </div>
+    }
+    return <div>
+      <h1>{this.container.name}</h1>
+    </div>
+  }
+}
+
+
 class Node extends Component {
   constructor(props) {
     super(props);
-    this.container = props.container.container;
-    console.log(this.container);
+    this.container = props.container;
+    this.onSelectContainer = props.onSelectContainer;
+    
     this.state = {
       collapse: NODE_COLLAPSED,
       children: []
@@ -21,9 +45,13 @@ class Node extends Component {
 
     this.handleExpand = this.handleExpand.bind(this);
     this.handleCollapse = this.handleCollapse.bind(this);
+    this.handleNameClick = this.handleNameClick.bind(this);
   }
 
   handleExpand() {
+    if (this.state === NODE_EXPANDED) {
+      return;
+    }
     this.setState({ collapse: NODE_LOADING });
     axios.get("/api/containers", { params: { parent: this.container.id } })
       .then(res => {
@@ -55,19 +83,24 @@ class Node extends Component {
 
   getChildrenView() {
     if (this.state.collapse === NODE_EXPANDED) {
-      if (this.state.children.length == 0) {
+      if (this.state.children.length === 0) {
         return <li className="text-muted"><em>No subcontainers.</em></li>
       }
       return this.state.children.map(container =>
-        <Node container={{container}}/>
+        <Node container={container} onSelectContainer={this.onSelectContainer}/>
       )
     }
     return <></>
   }
 
+  handleNameClick() {
+    this.handleExpand()
+    this.onSelectContainer(this.container);
+  }
+
   render() {
     return <li key="{this.container.id}">
-      <div>{this.getArrow()} {this.container.name}</div>
+      <div>{this.getArrow()} <button className="btn btn-sm btn-link" onClick={this.handleNameClick}>{this.container.name}</button></div>
       <ul className="container-tree">
         {this.getChildrenView()}
       </ul>
@@ -76,9 +109,10 @@ class Node extends Component {
 }
 
 
-class ObjectBrowser extends Component {
+class ContainerHiearchyBrowser extends Component {
   constructor(props) {
     super(props);
+    this.onSelectContainer = props.onSelectContainer;
     this.state = {
       dataActive: false,
       rootContainers: []
@@ -99,25 +133,47 @@ class ObjectBrowser extends Component {
   render() {
     if (this.state.dataActive) {
       return <ul className="container-tree">
-        {this.state.rootContainers.map(container => <Node container={{container}}/>)}
+        {this.state.rootContainers.map(container => <Node container={container} onSelectContainer={this.onSelectContainer}/>)}
       </ul>
-    } else {
-      return <div>
-        <div className="d-flex justify-content-center">
-          <div className="spinner-border text-primary m-5" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
+    }
+    return <div>
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border text-primary m-5" role="status">
+          <span className="sr-only">Loading...</span>
         </div>
       </div>
-    }
+    </div>
   }
 }
 
 
 class Browser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedContainer: null
+    }
+
+    this.onSelectContainer = this.onSelectContainer.bind(this);
+  }
+
+  onSelectContainer(container) {
+    this.setState({
+      selectedContainer: container
+    });
+  }
+
   render() {
     return <div className="container-fluid">
-      <ObjectBrowser/>
+      <div className="row flex-xl-nowrap">
+        <div className="col-12 col-md-3 col-xl-2 bd-sidebar">
+          <ContainerHiearchyBrowser onSelectContainer={this.onSelectContainer} />
+        </div>
+        <main className="col-12 col-md-9 col-xl-flex bd-content">
+          <ContainerViewer container={this.state.selectedContainer} />
+        </main>
+      </div>
     </div>
   }
 }
