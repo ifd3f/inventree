5432808e95068c6e06f902f95ebfc858249f3763
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-01-03 12:50:20 -0800

    URL-based browser state

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function MiniSearch(props) {
   return (
@@ -15,11 +21,14 @@ function Navbar(props) {
   return (
     <header className="app-header navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
       <span className="navbar-brand mr-0 mr-md-2" role="button">
-        <h2 class="text-small">Unrefined Stockpile</h2>
+        <Link to="/"><h2>Unrefined Stockpile</h2></Link>
       </span>
       <span className="navbar-nav bd-navbar-nav flex-row" role="button">
         <li className="nav-item">
-          <a className="nav-link active">Browse</a>
+          <Link to="/" className="nav-link active">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/browse" className="nav-link active">Browse</Link>
         </li>
       </span>
       <ul className="navbar-nav ml-md-auto d-md-flex">
