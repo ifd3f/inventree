5c616fe5a4e7d517a4ad7d75a154f802b8912f2d
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-01-05 16:27:14 -0800

    Refactorings

--- frontend/src/detail/ModelDetail.js
+++ frontend/src/detail/ModelDetail.js
@@ -1,14 +1,8 @@
-import React, { Component } from "react";
-import {MaybeNameURL, MaybeNotProvided} from "./util.js"
+import React, {Component} from "react";
+import {MaybeNotProvided} from "../util.js"
 import axios from "axios";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
-import { Container } from "reactstrap";
+import {useParams} from "react-router-dom";
+import {FetchedContainerContentsTable} from "./ContentsTable";
 
 
 export function RoutedContainerDetail(props) {
@@ -76,25 +70,6 @@ export class DynamicallyLoadedContainerDetail extends Component {
 }
 
 
-function ItemRow(props) {
-  let item = props.item
-  return <tr key={`i-${item.id}`}>
-    <td>{`${props.item.name} x ${props.item.quantity}`}</td>
-    <td>Item</td>
-    <td>{props.item.description}</td>
-  </tr>
-}
-
-function ContainerRow(props) {
-  let container = props.container
-  return <tr key={`c-${container.id}`}>
-    <td><Link to={`/browse/${container.id}`}>{container.name}</Link></td>
-    <td>Container</td>
-    <td>{container.description}</td>
-  </tr>
-}
-
-
 function ContainerInfoCard(props) {
   let container = props.container;
   let imageSection = null;
@@ -123,106 +98,6 @@ function ContainerInfoCard(props) {
       </div>
     </div>
   );
-}
-
-
-function StatelessContainerContentsTable(props) {
-  let body;
-  if (props.contents) {
-    let containers = props.contents.containers.map(container => <ContainerRow container={container} />);
-    let items = props.contents.items.map(item => <ItemRow item={item} />);
-    body = <>
-      {containers}
-      {items}
-    </>;
-  }
-  if (!body) {
-    body = <p className="text-muted">This container is empty.</p>;
-  }
-
-  return <table className="table">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Type</th>
-        <th>Description</th>
-      </tr>
-    </thead>
-    <tbody>
-      {body}
-    </tbody>
-  </table>
-}
-
-class FetchedContainerContentsTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: props.id,
-      containersFetched: false,
-      containers: [],
-      itemsFetched: false,
-      items: []      
-    }
-  }
-  static getDerivedStateFromProps(props, prevState) {
-    if (!prevState || props.id !== prevState.id) {
-      return {
-        id: props.id,
-        containersFetched: false,
-        containers: [],
-        itemsFetched: false,
-        items: []
-      }
-    }
-    return null;
-  }
-
-  componentDidMount() {
-    this.loadContainers();
-    this.loadItems();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.state)
-    if (!this.state.containersFetched) {
-      this.loadContainers();
-    }
-    if (!this.state.itemsFetched) {
-      this.loadItems();
-    }
-  }
-
-  loadContainers() {
-    axios.get(`/api/containers/${this.state.id}/children`)
-      .then(res => {
-        this.setState({
-          containersFetched: true,
-          containers: res.data
-        })
-      });
-  }
-
-  loadItems() {
-    axios.get(`/api/containers/${this.state.id}/items`)
-      .then(res => {
-        this.setState({
-          itemsFetched: true,
-          items: res.data
-        })
-      });
-  }
-
-  render() {
-    if (this.state.containersFetched && this.state.itemsFetched) {
-      return <StatelessContainerContentsTable contents={{items: this.state.items, containers: this.state.containers}} />
-    } else {
-      return <div className="spinner-border text-primary m-5" role="status">
-        <span className="sr-only">Loading...</span>
-      </div>
-    }
-  }
-
 }
 
 
