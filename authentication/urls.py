b9f07ae7db08c60d3bfb524b994aff4497a13b3c
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-01-10 22:21:51 -0800

    Move login auth to authentication app

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('csrf/', views.csrf),
     path('ping/', views.ping),
+    url(r'^rest-auth/', include('rest_auth.urls')),
 ]
