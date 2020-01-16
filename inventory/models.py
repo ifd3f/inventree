f891a0dc112350a050a275d32b396a361e870b53
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-01-15 22:54:57 -0800

    UUID system and document container_type

--- inventory/models.py
+++ inventory/models.py
@@ -1,12 +1,52 @@
+import uuid
+
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 from jsonfield import JSONField
 
-
 CONTAINER_TYPE_DEFAULT = 0
+"""
+Specifies a container that has no defined structure. The contents are rendered as a table.
+
+container_metadata = {}
+
+child location_metadata schema:
+    
+    - location: str -> a description of its location in the container
+"""
+
 CONTAINER_TYPE_GRID = 1
+"""
+Specifies a container with a grid structure. The container has an integer number of rows and columns. Each child of the 
+container occupies a cell in the grid, and there can be multiple children in a single cell. There is a special cell
+outside of the grid specifying unsorted children.
+
+container_metadata schema: 
+    
+    - rows: int
+    - cols: int
+
+child location_metadata schema:
+
+    - position: null | {x: int, y: int} -> null means unsorted, object means sorted
+"""
+
 CONTAINER_TYPE_FREEFORM = 2
+"""
+Specifies a container with a non-gridlike structure. The container has a real width and height. Each child has a shape 
+and an (x, y) position inside the container, or it is unsorted.
+
+container_metadata schema:
+    
+    - width: float
+    - height: float
+
+child location_metadata schema:
+
+    - shape: {type: str, ...} -> type specifies the type of the object.
+    - pos: null | {x: float, y: float} -> where it is, or null if unsorted.  
+"""
 
 CONTAINER_TYPE_CHOICES = [
     (CONTAINER_TYPE_DEFAULT, 'default'),
@@ -22,6 +62,25 @@ class Node(models.Model):
 
     parent = models.ForeignKey('Container', on_delete=models.SET_NULL, blank=True, null=True)
     location_metadata = JSONField(default={}, blank=True)
+    """
+    Used for custom positioning.
+    """
+
+    qr_uuid = models.UUIDField(default=uuid.uuid1, db_index=True)
+    """
+    This is the UUID that will be used to universally identify this object, no matter what inventory system it's 
+    attached to. It is initialized to a default value, but it can be changed to any value you want. 
+    
+    There are 2 ways to use QR UUIDs:
+
+    - **Use the default UUID:** Print out the default sticker, and stick it on the object, then you're done.
+    - **Use a UUID from a sheet:** The server can generate sheets of multiple unassociated stickers. Every time you 
+    put a sticker on an object, you scan it to register the item's new UUID. 
+    """
+
+    @property
+    def qr_uri(self):
+        return f'unrefined-stockpile-qr:{self.qr_uuid}'
 
     class Meta:
         abstract = True
@@ -29,6 +88,9 @@ class Node(models.Model):
 
 class Container(Node):
     container_type = models.IntegerField(choices=CONTAINER_TYPE_CHOICES, default=CONTAINER_TYPE_DEFAULT)
+    """
+    """
+
     metadata = JSONField(default={})
 
     @property
@@ -38,6 +100,10 @@ class Container(Node):
     @property
     def type_verbose(self):
         return CONTAINER_TYPE_CHOICES[self.container_type]
+
+    @property
+    def resource_path(self):
+        return f'/container/{self.id}'
 
     def __str__(self):
         return self.name
@@ -83,6 +149,10 @@ class Item(Node):
 
     @property
     def link(self):
+        return f'/item/{self.id}'
+
+    @property
+    def resource_path(self):
         return f'/item/{self.id}'
 
     def __str__(self):
