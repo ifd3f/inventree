8c8a572ea3f8c760622fd912b3b1776bb9c885ae
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-01-16 22:27:24 -0800

    Move container search to a new directory

---
+++ frontend/src/util/ContainerSearch.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+import axios from "axios";
+import {AsyncTypeahead} from "react-bootstrap-typeahead";
+
+export function ContainerSearch(props) {
+    const name = props.name;
+    const onChange = props.onChange;
+    const defaultValue = props.defaultValue;
+    const [wasChanged, setWasChanged] = useState(false);
+    const [options, setOptions] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSearch = (query) => {
+        setIsLoading(true);
+        axios.get("/api/containers/search", {
+            params: {
+                'text__contains': query
+            }
+        }).then(res => {
+            setOptions(res.data);
+            setIsLoading(false);
+        });
+    };
+
+    const handleChange = (options) => {
+        onChange({
+            name: name,
+            option: options.length === 0 ? null : options[0]
+        });
+    };
+
+    const handleInputChange = (query) => {
+        setWasChanged(true);
+    };
+
+    const selected = (wasChanged || !defaultValue) ? null : [defaultValue];
+
+    return <AsyncTypeahead
+        id="search-bar"
+        minLength={2}
+        labelKey={option => option.name}
+        placeholder="Containers, items, tags..."
+        onSearch={handleSearch}
+        options={options}
+        isLoading={isLoading}
+        onChange={handleChange}
+        onInputChange={handleInputChange}
+        selected={selected}
+    />
+}
