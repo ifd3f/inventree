c910d05a8e4dd7e1333adb51ada84adede268943
Astrid Augusta Yu <astridyu3.14@gmail.com>
2020-05-24 13:59:51 -0700

    lmao typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Switch} from "react-router-dom";
+import {CookiesProvider} from "react-cookie";
+import {Container} from "@material-ui/core";
+
+
+function App() {
+    return <Router>
+        <CookiesProvider>
+            <Switch>
+                <Container maxWidth={"xl"}>
+                    foo
+                </Container>
+            </Switch>
+        </CookiesProvider>
+    </Router>;
+}
+
+export default App;
+  
